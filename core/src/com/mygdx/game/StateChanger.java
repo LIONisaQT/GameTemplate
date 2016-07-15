cd80b77a2cefc8dba209659a15db4a82975c44bb
tomhsu1 <tom268@gmail.com>
2016-07-15 11:49:38 -0700

    added levels

--- core/src/com/mygdx/game/StateChanger.java
+++ core/src/com/mygdx/game/StateChanger.java
@@ -12,7 +12,7 @@ public class StateChanger extends Button {
     }
 
     public void action() {
-        if (MyGdxGame.state == MyGdxGame.GameState.START) MyGdxGame.state = MyGdxGame.GameState.IN_GAME;
+        if (MyGdxGame.state == MyGdxGame.GameState.START) MyGdxGame.state = MyGdxGame.GameState.LEVEL_SELECT;
         else if (MyGdxGame.state == MyGdxGame.GameState.GAME_OVER) {
             MyGdxGame.state = MyGdxGame.GameState.START;
         }
